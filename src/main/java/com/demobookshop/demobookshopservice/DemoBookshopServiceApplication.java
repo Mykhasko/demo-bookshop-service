ab222470bf18dd57a64a7f27bff05a02a22ecc9c
Viktor Mykhasko <mikhasko@deskit.ru>
2023-11-20 21:39:15 +0200

    Initial commit.

---
+++ src/main/java/com/demobookshop/demobookshopservice/DemoBookshopServiceApplication.java
@@ -0,0 +1,13 @@
+package com.demobookshop.demobookshopservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBookshopServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBookshopServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demobookshop/demobookshopservice/DemoBookshopServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demobookshop.demobookshopservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBookshopServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
