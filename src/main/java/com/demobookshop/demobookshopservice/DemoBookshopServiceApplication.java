41bca9f26e75cd1422d551db3ac1a88c6013e5a5
Viktor Mykhasko <mikhasko@deskit.ru>
2025-07-12 15:17:32 +0300

    feat(#12): Add description Open API.

--- src/main/java/com/demobookshop/demobookshopservice/DemoBookshopServiceApplication.java
+++ src/main/java/com/demobookshop/demobookshopservice/DemoBookshopServiceApplication.java
@@ -1,8 +1,25 @@
 package com.demobookshop.demobookshopservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Demo Bookshop Service API",
+				version = "1.0.0",
+				description = "API for managing books in a demo bookshop microservice application.",
+				contact = @Contact(
+						name = "Demo Bookshop Support",
+						email = "it.support@demobookshop.com"),
+				license = @License(
+						name = "Apache 2.0",
+						url = "http://www.apache.org/licenses/LICENSE-2.0.html")
+		)
+)
 @SpringBootApplication
 public class DemoBookshopServiceApplication {
 
