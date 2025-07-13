974f5f38ed716349516c02c6acc3d9f1bc00d9b1
Viktor Mykhasko <mikhasko@deskit.ru>
2025-07-13 15:07:08 +0300

    style(#12):Format source file.

--- src/test/java/com/demobookshop/demobookshopservice/TestDemoBookshopServiceApplication.java
+++ src/test/java/com/demobookshop/demobookshopservice/TestDemoBookshopServiceApplication.java
@@ -10,14 +10,15 @@ import org.testcontainers.utility.DockerImageName;
 @TestConfiguration(proxyBeanMethods = false)
 public class TestDemoBookshopServiceApplication {
 
-	@Bean
-	@ServiceConnection
-	PostgreSQLContainer<?> postgresContainer() {
-		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
-	}
+  @Bean
+  @ServiceConnection
+  PostgreSQLContainer<?> postgresContainer() {
+    return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+  }
 
-	public static void main(String[] args) {
-		SpringApplication.from(DemoBookshopServiceApplication::main).with(TestDemoBookshopServiceApplication.class).run(args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.from(DemoBookshopServiceApplication::main)
+        .with(TestDemoBookshopServiceApplication.class)
+        .run(args);
+  }
 }
