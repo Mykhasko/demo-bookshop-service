cace354059d6ea9dbd1109b42638dc9246791c0c
Viktor Mykhasko <mikhasko@deskit.ru>
2025-07-13 15:00:10 +0300

    style(#12): Reformat source files by Java Code Style .

--- src/main/java/com/demobookshop/demobookshopservice/DemoBookshopServiceApplication.java
+++ src/main/java/com/demobookshop/demobookshopservice/DemoBookshopServiceApplication.java
@@ -8,23 +8,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @OpenAPIDefinition(
-		info = @Info(
-				title = "Demo Bookshop Service API",
-				version = "1.0.0",
-				description = "API for managing books in a demo bookshop microservice application.",
-				contact = @Contact(
-						name = "Demo Bookshop Support",
-						email = "it.support@demobookshop.com"),
-				license = @License(
-						name = "Apache 2.0",
-						url = "http://www.apache.org/licenses/LICENSE-2.0.html")
-		)
-)
+    info =
+        @Info(
+            title = "Demo Bookshop Service API",
+            version = "1.0.0",
+            description = "API for managing books in a demo bookshop microservice application.",
+            contact =
+                @Contact(name = "Demo Bookshop Support", email = "it.support@demobookshop.com"),
+            license =
+                @License(
+                    name = "Apache 2.0",
+                    url = "http://www.apache.org/licenses/LICENSE-2.0.html")))
 @SpringBootApplication
 public class DemoBookshopServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoBookshopServiceApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(DemoBookshopServiceApplication.class, args);
+  }
 }
