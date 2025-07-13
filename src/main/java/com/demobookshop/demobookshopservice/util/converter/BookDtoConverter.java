2e7ca2372b35effee052905dd224143bf05b28f9
Viktor Mykhasko <mikhasko@deskit.ru>
2025-07-13 15:04:24 +0300

    feat(#12):Add converter from BookDTO to Book Entity. Reformat code.

--- src/main/java/com/demobookshop/demobookshopservice/util/converter/BookDtoConverter.java
+++ src/main/java/com/demobookshop/demobookshopservice/util/converter/BookDtoConverter.java
@@ -2,13 +2,12 @@ package com.demobookshop.demobookshopservice.util.converter;
 
 import com.demobookshop.demobookshopservice.model.Book;
 import com.demobookshop.demobookshopservice.model.dto.BookDto;
-
 import java.util.Objects;
 import java.util.function.Function;
 
 /**
- * Utility class for converting Book entities to Book DTOs.
- * This class provides a static function to convert a Book object to a BookDto object.
+ * Utility class for converting Book entities to Book DTOs. This class provides a static function to
+ * convert a Book object to a BookDto object.
  */
 public class BookDtoConverter {
 
@@ -22,12 +21,27 @@ public class BookDtoConverter {
    * @param b the Book entity to convert
    * @return a BookDto representation of the Book entity
    */
-  public static final Function<Book, BookDto> bookToBookDto = b -> new BookDto(b.getId(),
+  public static final Function<Book, BookDto> bookToBookDto = b -> new BookDto(
+      b.getId(),
       b.getUuid().toString(),
       b.getParentId(),
-      Objects.isNull( b.getParentUuid()) ? null : b.getParentUuid().toString(),
+      Objects.isNull(b.getParentUuid()) ? null : b.getParentUuid().toString(),
       b.isCatalog(),
       b.getTitle(),
       b.getAuthor());
 
+  /**
+   * Function to convert a BookDto to a Book entity.
+   *
+   * @param b the BookDto to convert
+   * @return a Book entity representation convert from the BookDto
+   */
+  public static final Function<BookDto, Book> bookDtoToBook = b -> new Book(
+      null,
+      Objects.isNull(b.uuid()) ? null : java.util.UUID.fromString(b.uuid()),
+      b.parentId(),
+      Objects.isNull(b.parentUuid()) ? null : java.util.UUID.fromString(b.parentUuid()),
+      b.isCatalog(),
+      b.title(),
+      b.author());
 }
