3a1629d3107db94457a4a459f2d6a09d7cdc4e58
Viktor Mykhasko <mikhasko@deskit.ru>
2025-07-13 15:03:07 +0300

    feat(#12):Add BookService and tests.

--- src/test/java/com/demobookshop/demobookshopservice/service/BookServiceTest.java
+++ src/test/java/com/demobookshop/demobookshopservice/service/BookServiceTest.java
@@ -1,20 +1,20 @@
 package com.demobookshop.demobookshopservice.service;
 
+import static com.demobookshop.demobookshopservice.util.converter.BookDtoConverter.bookDtoToBook;
+import static com.demobookshop.demobookshopservice.util.converter.BookDtoConverter.bookToBookDto;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 import com.demobookshop.demobookshopservice.model.Book;
 import com.demobookshop.demobookshopservice.model.dto.BookDto;
 import com.demobookshop.demobookshopservice.repository.BookRepository;
+import java.util.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import java.util.*;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
-
 
 @ExtendWith(MockitoExtension.class)
 class BookServiceTest {
@@ -29,8 +29,7 @@ class BookServiceTest {
     return new ArrayList<Book>(List.of(
         new Book(1L, UUID.randomUUID(), 0L, null, true, "Title 1", "Author 1"),
         new Book(2L, UUID.randomUUID(), 0L, null, false, "Title 2", "Author 2"),
-        new Book(3L, UUID.randomUUID(), 0L, null, true, "Title 3", "Author 3")
-    ));
+        new Book(3L, UUID.randomUUID(), 0L, null, true, "Title 3", "Author 3")));
   }
 
   @Test
@@ -54,12 +53,81 @@ class BookServiceTest {
     when(bookRepository.findById(bookId)).thenReturn(Optional.of(book));
 
     // Act
-    Optional<BookDto> bookDto = bookService.getBookById(bookId);
+    BookDto bookDto = bookService.getBookById(bookId);
 
     // Assert
-    assertTrue(bookDto.isPresent(), "Book should be present");
-    assertEquals(bookId, bookDto.get().id(), "Book ID should match");
-    assertEquals("Title 1", bookDto.get().title(), "Book title should match");
-    assertEquals("Author 1", bookDto.get().author(), "Book author should match");
+    assertNotNull(bookDto, "Book should be present");
+    assertEquals(bookId, bookDto.id(), "Book ID should match");
+    assertEquals("Title 1", bookDto.title(), "Book title should match");
+    assertEquals("Author 1", bookDto.author(), "Book author should match");
+  }
+
+  @Test
+  void addBook() {
+    // Arrange
+    BookDto newBookDto =
+        new BookDto(null, UUID.randomUUID().toString(), 0L, null, true, "New Title", "New Author");
+    Book newBook = bookDtoToBook.apply(newBookDto);
+    Book savedBook = bookDtoToBook.apply(newBookDto);
+    savedBook.setId(4L); // Simulate the ID being set after saving
+    BookDto savedBookDto = bookToBookDto.apply(savedBook);
+    when(bookRepository.save(newBook)).thenReturn(savedBook);
+    // Act
+    BookDto addedBookDto = bookService.addBook(newBookDto);
+    // Assert
+    assertNotNull(addedBookDto, "Book should not be null");
+    assertEquals(savedBookDto.id(), addedBookDto.id(), "Added book ID should match 4L");
+    assertEquals(
+        0,
+        newBookDto.title().compareToIgnoreCase(addedBookDto.title()),
+        "Added book title should match");
+
+    verify(bookRepository, times(1)).save(newBook);
+    verifyNoMoreInteractions(bookRepository);
+  }
+
+  @Test
+  void updateBook() {
+    // Arrange
+    Long bookId = 1L;
+    var updatedTitle = "Updated Title";
+    var updatedAuthor = "Updated Author";
+    var updatedUuid = UUID.randomUUID().toString();
+    BookDto updatedBookDto =
+        new BookDto(3L, updatedUuid, 0L, null, true, updatedTitle, updatedAuthor);
+    Book existingBook =
+        new Book(bookId, UUID.randomUUID(), 0L, null, true, "Old Title", "Old Author");
+    Book updatedBook = bookDtoToBook.apply(updatedBookDto);
+    updatedBook.setId(bookId); // Ensure the ID matches the existing book
+    // Mock the repository behavior
+    when(bookRepository.findById(bookId)).thenReturn(Optional.of(existingBook));
+    when(bookRepository.save(any(Book.class))).thenReturn(updatedBook);
+
+    // Act
+    BookDto result = bookService.updateBook(bookId, updatedBookDto);
+
+    // Assert
+    assertNotNull(result, "Updated book should not be null");
+    assertEquals(bookId, result.id(), "Updated book ID should match");
+    assertEquals(updatedTitle, result.title(), "Updated book title should match");
+    assertEquals(updatedAuthor, result.author(), "Updated book author should match");
+
+    verify(bookRepository, times(1)).findById(bookId);
+    verify(bookRepository, times(1)).save(any(Book.class));
+    verifyNoMoreInteractions(bookRepository);
+  }
+
+  @Test
+  void deleteBook() {
+    // Arrange
+    Long bookId = 1L;
+    doNothing().when(bookRepository).deleteById(bookId);
+
+    // Act
+    bookService.deleteBook(bookId);
+
+    // Assert
+    verify(bookRepository, times(1)).deleteById(bookId);
+    verifyNoMoreInteractions(bookRepository);
   }
 }
