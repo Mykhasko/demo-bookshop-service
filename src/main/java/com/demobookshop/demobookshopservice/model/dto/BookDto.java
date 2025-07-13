c1cbbcf435797afb8c547dd54875be55e5112bb6
Viktor Mykhasko <mikhasko@deskit.ru>
2025-07-13 14:36:24 +0300

    style(#12): Reformat BookDto source file.

--- src/main/java/com/demobookshop/demobookshopservice/model/dto/BookDto.java
+++ src/main/java/com/demobookshop/demobookshopservice/model/dto/BookDto.java
@@ -2,12 +2,12 @@ package com.demobookshop.demobookshopservice.model.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
-
 import java.io.Serial;
 import java.io.Serializable;
 
 /**
  * Book Data Transfer Object (DTO).
+ *
  * @param id book ID
  * @param uuid book UUID
  * @param parentId reference on parent entity ID
@@ -18,28 +18,21 @@ import java.io.Serializable;
  */
 @Schema(name = "Book", description = "Book DTO")
 public record BookDto(
-    @Schema(description = "Book ID", example = "1")
-    @JsonProperty("id")
-    Long id,
+    @Schema(description = "Book ID", example = "1") @JsonProperty("id") Long id,
     @Schema(description = "Book UUID", example = "123e4567-e89b-12d3-a456-426614174000")
-    @JsonProperty("uuid")
-    String uuid,
-    @Schema(description = "Parent ID", example = "1")
-    @JsonProperty("parentId")
-    Long parentId,
+        @JsonProperty("uuid")
+        String uuid,
+    @Schema(description = "Parent ID", example = "1") @JsonProperty("parentId") Long parentId,
     @Schema(description = "Parent UUID", example = "123e4567-e89b-12d3-a456-426614174000")
-    @JsonProperty("parentUuid")
-    String parentUuid,
-    @Schema(description = "Element is Catalog", example = "true")
-    @JsonProperty("isCatalog")
-    boolean isCatalog,
-    @Schema(description = "Book Title", example = "The Great Gatsby")
-    @JsonProperty("title")
-    String title,
-    @Schema(description = "Book Author", example = "F. Scott Fitzgerald")
-    @JsonProperty("author")
-    String author
-) implements Serializable {
+        @JsonProperty("parentUuid")
+        String parentUuid,
+    @Schema(description = "Element is Catalog", example = "true") @JsonProperty("isCatalog")
+        boolean isCatalog,
+    @Schema(description = "Book Title", example = "The Great Gatsby") @JsonProperty("title")
+        String title,
+    @Schema(description = "Book Author", example = "F. Scott Fitzgerald") @JsonProperty("author")
+        String author)
+    implements Serializable {
   @Serial
   private static final long serialVersionUID = 12L;
 }
